4e136d920b546dc0333d2d2f40a9d498561bb99a
Mustapha Mohammed <mustaphamohammed0705@gmail.com>
2024-06-30 15:30:21 +0000

    Updated the def do_create(self, arg): function of the command interpreter (console.py) to allow for object creation with given parameters:

--- console.py
+++ console.py
@@ -73,8 +73,8 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
+                    if pline[0] == '{' and pline[-1] =='}'\
+                            and type(eval(pline)) == dict:
                         _args = pline
                     else:
                         _args = pline.replace(',', '')
@@ -113,18 +113,88 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    # def do_create(self, args):
+    #     """ Create an object of any class and allow for
+    #     object creation with given parameters:
+    #     create <Class name> <param 1> <param 2> <param 3>...
+    #     """
+    #     arguments = args.split()
+    #     c_name = arguments[0]
+    #     # print(c_name)
+    #     array = []
+    #     # Iterate through a copy of arguments to avoid modifying it while iterating
+    #     for arg in arguments[1:]:
+    #         if '=' in arg:
+    #             key, value = arg.split('=')
+    #             # Remove quotes around the value if they exist
+    #             if value.startswith('"') and value.endswith('"'):
+    #                 value = value[1:-1]
+                    
+    #             array.append((key, value))
+    #     # print(array)
+    #     if not c_name:
+    #         print("** class name missing **")
+    #         return
+    #     elif c_name not in HBNBCommand.classes:
+    #         print("** class doesn't exist **")
+    #         return
+        
+    #     new_instance = HBNBCommand.classes[c_name]()
+    #     for key, value in array:
+    #         setattr(new_instance, key, value)
+    #     # print(new_instance)
+    #     storage.save()
+    #     print(new_instance.id)
+    #     storage.save()
+    
     def do_create(self, args):
-        """ Create an object of any class"""
+        """ Create an object of any class and allow for
+        object creation with given parameters:
+        create <Class name> <param 1> <param 2> <param 3>...
+        """
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        
+        arguments = args.split()
+        c_name = arguments[0]
+        
+        if c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
+        
+        # Extract parameters from arguments
+        array = []
+        for arg in arguments[1:]:
+            if '=' in arg:
+                key, value = arg.split('=', 1)  # split only on the first '='
+                # Handle string values
+                if value.startswith('"') and value.endswith('"'):
+                    value = value[1:-1].replace('\\"', '"').replace('_', ' ')
+                # Handle float values
+                elif '.' in value:
+                    try:
+                        value = float(value)
+                    except ValueError:
+                        continue
+                # Handle integer values
+                else:
+                    try:
+                        value = int(value)
+                    except ValueError:
+                        continue
+                array.append((key, value))
+        
+        new_instance = HBNBCommand.classes[c_name]()
+        
+        # Set attributes for the new instance
+        for key, value in array:
+            setattr(new_instance, key, value)
+        
+        # Save the new instance to storage
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
+
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +342,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +350,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
